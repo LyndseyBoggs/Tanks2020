baeeec90d74a9ac21c578c1e017e434e3f7460c8
Lyndsey Boggs <lynboggs@uat.edu>
2020-01-29 13:16:30 -0700

    Day 7: Advanced Waypoints w/ LoopType

--- Assets/Scripts/SampleAIController.cs
+++ Assets/Scripts/SampleAIController.cs
@@ -13,7 +13,17 @@ public class SampleAIController : MonoBehaviour
     private Transform tf; 
     public int currentWayPoint = 0;
     public float closeEnough; 
-    
+
+    public enum LoopType
+    {
+        Stop,
+        Loop,
+        Pingpong
+    }
+
+    public LoopType loopType;
+    public bool isPingpongForward = true; //tracks if tank is pingponging forward or backward.
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,11 +35,13 @@ public class SampleAIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //If not currently turning to look at waypoint
         if (motor.RotateTowards(waypoints[currentWayPoint].position, data.rotateSpeed))
         {
             //Do Nothing!
         }
 
+        //Move forward
         else
         {
             motor.Move(data.moveSpeed);
@@ -38,21 +50,87 @@ public class SampleAIController : MonoBehaviour
         //If close enough to the waypoint, progress
         if (Vector3.SqrMagnitude(waypoints[currentWayPoint].position - tf.position) < (closeEnough * closeEnough))
         {
-            //if not at the end of the list (avoid index out of range)
-            if (currentWayPoint <= waypoints.Length)
+            switch (loopType)
             {
-                //Target next waypoint in list
-                currentWayPoint++;
-            }
+                case LoopType.Stop:
+                    //if next waypoint will not exceed the array (avoid index out of range)
+                    if (currentWayPoint + 1 < waypoints.Length)
+                    {
+                        //Target next waypoint
+                        currentWayPoint++;
+                    }
+                    break;
 
-            //else, we are at end of waypoint list
-            else
-            {
-                //if pingpong
+                case LoopType.Loop:
+                    //if next waypoint will not exceed the array (avoid index out of range)
+                    if (currentWayPoint + 1 < waypoints.Length)
+                    {
+                        //Target next waypoint
+                        currentWayPoint++;
+                    }
 
-                //if loop
-                currentWayPoint = 0; // return to first waypoint
+                    //else, we are at end of waypoint list
+                    else
+                    {
+                        //if pingpong
+
+                        //if loop
+                        currentWayPoint = 0; // return to first waypoint
+                    }
+                    break;
+
+                case LoopType.Pingpong:
+
+                    //PingPong Forward
+                    if (isPingpongForward == true)
+                    {
+                        //if next waypoint will not exceed the array (avoid index out of range)
+                        if (currentWayPoint + 1 < waypoints.Length)
+                        {
+                            //Target next waypoint
+                            currentWayPoint++;
+                        }
+
+                        //else, we are at end of waypoint list -> pingpong backwards now
+                        else
+                        {
+                            isPingpongForward = false;
+
+                            //if loop
+                            currentWayPoint--; // return to first waypoint
+                        }
+                    }
+
+                    //PingPong Backwards
+                    else
+                    {
+                        //if next waypoint will not exceed the array beginning(avoid index out of range)
+                        if (currentWayPoint - 1 >= 0)
+                        {
+                            //Target next reverse waypoint
+                            currentWayPoint--;
+                        }
+
+                        //else, we are at start of waypoint list -> pingpong forwards now
+                        else
+                        {
+                            isPingpongForward = true;
+
+                            //if loop
+                            currentWayPoint++; // return to first waypoint
+                        }
+                    }
+                    
+
+                    
+                    break;
+
+                default:
+                    Debug.Log("Loop type not implemented.");
+                    break;
             }
+            
+            
             
             
         }
